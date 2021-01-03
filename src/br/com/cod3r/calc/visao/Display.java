55e02537c7da4660f42601545b843c3239df820e
Jhonatan Medeiros <55262464+iamjhonatan@users.noreply.github.com>
2021-01-03 18:55:13 -0300

    Final da implementação do projeto.

---
+++ src/br/com/cod3r/calc/visao/Display.java
@@ -0,0 +1,29 @@
+package br.com.cod3r.calc.visao;
+
+import br.com.cod3r.calc.modelo.Memoria;
+import br.com.cod3r.calc.modelo.MemoriaObservador;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JPanel implements MemoriaObservador {
+
+    private final JLabel label;
+
+    public Display() {
+        Memoria.getInstancia().adicionarObservador(this); // Notificando sempre que o valor for modificado
+
+        setBackground(new Color(46,49,50));
+        label = new JLabel(Memoria.getInstancia().getTextoAtual());
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("courier", Font.PLAIN, 30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+
+        add(label);
+    }
+
+    public void valorAlterado (String novoValor){
+        label.setText(novoValor);
+    }
+}
